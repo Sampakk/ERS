c540d0a5b681534d0c0bb21433653c427e6108b7
Naikoun PC <91052420+Niko-Viitio@users.noreply.github.com>
2022-02-19 11:06:38 +0200

    Headbobbing & fov kick

--- Assets/Scripts/CameraEffects.cs
+++ Assets/Scripts/CameraEffects.cs
@@ -7,19 +7,52 @@ public class CameraEffects : MonoBehaviour
     Camera cam;
     Player player;
 
-    public float fovKickAmount = 5f;
+    public float headbobSpeed = 8f;
+
+    public float fovKickAmount = 6f;
     float normalFov;
+
+    Vector3 normalPos;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponentInParent<Player>();
-        cam = GetComponent<Camera>();
+        cam = GetComponentInChildren<Camera>();
+
+        normalFov = cam.fieldOfView;
+        normalPos = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FovKicking();
+
+        HeadBobbing();
+    }
+
+    void FovKicking()
+    {
+        //Get target fov amount
+        float fovTarget = (player.IsRunning()) ? normalFov + fovKickAmount : normalFov;
+
+        //Lerp fov to target
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, fovTarget, 8f * Time.deltaTime);
+    }
+
+    void HeadBobbing()
+    {
+        if (player.IsRunning())
+        {
+            float offset = Mathf.Sin(Time.time * headbobSpeed) * 0.1f;
+
+            Vector3 targetPos = new Vector3(0, normalPos.y + offset, 0);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, targetPos, 10f * Time.deltaTime);
+        }
+        else
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, normalPos, 10f * Time.deltaTime);
+        }
     }
 }
